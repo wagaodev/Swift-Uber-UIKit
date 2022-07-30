8b6f2b4945446230ff205a7290daab83dbdaf561
Wagão <wcfx.dev@gmail.com>
2022-07-30 17:50:35 -0300

    chore: change the config files

---
+++ uber-uikit/Controllers/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  uber-uikit
+//
+//  Created by Wagão on 30/07/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureUI()
+    }
+    
+    func configureUI(){
+        view.backgroundColor = .red
+    }
+
+}
